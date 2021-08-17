d301c27d6c0f0e372884055a89ce1e3ef09aabde
lamiproche <lamiproche@gmail.com>
2021-08-17 15:04:49 +0900

    log4j2 xml added

---
+++ src/main/java/org/zerock/bmini/controller/Sample.java
@@ -0,0 +1,4 @@
+package org.zerock.bmini.controller;
+
+public class Sample {
+}
---
+++ src/main/java/org/zerock/bmini/dto/boardDTO.java
@@ -0,0 +1,9 @@
+package org.zerock.bmini.dto;
+
+import java.sql.Timestamp;
+
+public class boardDTO {
+    private Long bno;
+    private String title, content, writer;
+    private Timestamp regdate;
+}
